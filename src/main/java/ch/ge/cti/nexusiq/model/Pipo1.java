e23f37a9e668095a06b39bcfd9cfd0796430bf75
Dubois-Pèlerin Yves (DIN) <yves.dubois-pelerin@etat.ge.ch>
2026-01-14 14:54:28 +0100

    Fix Sonar issues

---
+++ src/main/java/ch/ge/cti/nexusiq/model/Pipo1.java
@@ -0,0 +1,13 @@
+package ch.ge.cti.nexusiq.model;
+
+import lombok.Getter;
+
+@SuppressWarnings("java:S1258")
+@Getter
+public class Pipo1 {
+
+    private String applicationId;
+
+    private String reportDataUrl;
+
+}
---
+++ src/main/java/ch/ge/cti/nexusiq/model/Pipo2.java
@@ -0,0 +1,12 @@
+package ch.ge.cti.nexusiq.model;
+
+import lombok.Getter;
+
+@Getter
+public class Pipo2 {
+
+    private String applicationId;
+
+    private String reportDataUrl;
+
+}
